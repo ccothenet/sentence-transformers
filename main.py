5bb64d0a82ddbc734b64e8e7c180111348fd788a
ccothenet <come.cothenet@sia-partners.com>
2020-02-21 11:43:18 +0100

    [add] new main file to call train on frBERT

---
+++ main.py
@@ -0,0 +1,63 @@
+from torch.utils.data import DataLoader
+import math
+from sentence_transformers import models, losses
+from sentence_transformers import SentencesDataset, LoggingHandler, SentenceTransformer
+from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator, LabelAccuracyEvaluator
+from sentence_transformers.readers import *
+import logging
+from datetime import datetime
+
+if __name__ == "__main__":
+
+    # Read the dataset
+
+    model_name = 'roberta-large'
+    batch_size = 16
+    nli_reader = NLIDataReader('./AllNLI')
+    train_num_labels = nli_reader.get_num_labels()
+    model_save_path = './training_nli_' + model_name + '-' + datetime.now().strftime(
+        "%Y-%m-%d_%H-%M-%S")
+    num_epochs= 10
+
+
+    # "camembert-base" is the name of Camembert Model from Hugging Face
+    word_embedding_model = models.CamemBERT("camembert-base")
+    pooling_model = models.Pooling(768,
+                                   pooling_mode_mean_tokens=True,
+                                   pooling_mode_cls_token=False,
+                                   pooling_mode_max_tokens=False)
+    model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+
+    train_data = SentencesDataset(nli_reader.get_examples('train', max_examples=20),
+                                  model=model)
+    train_dataloader = DataLoader(train_data, shuffle=True,
+                                  batch_size=batch_size)
+    train_loss = losses.SoftmaxLoss(model=model,
+                                    sentence_embedding_dimension=model.get_sentence_embedding_dimension(),
+                                    num_labels=train_num_labels)
+
+    dev_data = SentencesDataset(nli_reader.get_examples('dev', max_examples=20),
+                                  model=model)
+    dev_dataloader = DataLoader(dev_data, shuffle=True,
+                                  batch_size=batch_size)
+    evaluator = LabelAccuracyEvaluator(train_dataloader)
+
+    warmup_steps = 4
+
+    model.fit(train_objectives=[(train_dataloader, train_loss)],
+              evaluator=evaluator,
+              epochs=num_epochs,
+              evaluation_steps=1000,
+              warmup_steps=warmup_steps,
+              output_path=model_save_path
+              )
+
+    model.save("model.pt")
+
+    # traubèdata = DataLoader("")
+
+    # TODO : Train on XNLI French dataset
+
+    # TODO : Déplacer Dossier git à la place de ce dossier Sentence_BERT
+
+    # TODO : https://github.com/UKPLab/sentence-transformers  Finetuner CamemBERT sur ce dataset
